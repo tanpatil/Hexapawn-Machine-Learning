5283a49ba79b9cb910688c37bccd4a02e2fa5b76
tanpatil <89934290+tanpatil@users.noreply.github.com>
2021-11-17 18:52:56 -0800

    added black formatting

--- hexapawn.py
+++ hexapawn.py
@@ -4,80 +4,82 @@ from copy import deepcopy
 import pickle
 import os
 
-WRAP=55
-WHITE=1
-BLACK=-1
-EMPTY=0
+WRAP = 55
+WHITE = 1
+BLACK = -1
+EMPTY = 0
+
 
 class HexpawnException(Exception):
     pass
 
+
 class IllegalCoordinate(HexpawnException):
     pass
+
 
 class IllegalMove(HexpawnException):
     pass
 
-fh=open("MovesetPermutations.txt","r")
-MoveSet=fh.readlines()
-InitialMoveSet=MoveSet[0:19]
-InitialMoveSetVar=''
+
+fh = open("MovesetPermutations.txt", "r")
+MoveSet = fh.readlines()
+InitialMoveSet = MoveSet[0:19]
+InitialMoveSetVar = ""
 for permutation in InitialMoveSet:
-    InitialMoveSetVar+=permutation.rstrip(',\n')+','
-InitialMoveSetVar=InitialMoveSetVar.rstrip(',')
-InitialMoves=tuple(eval(InitialMoveSetVar))
-MoveSetConfigs=MoveSet[19:38]
-ConfigMoveSetVar=''
+    InitialMoveSetVar += permutation.rstrip(",\n") + ","
+InitialMoveSetVar = InitialMoveSetVar.rstrip(",")
+InitialMoves = tuple(eval(InitialMoveSetVar))
+MoveSetConfigs = MoveSet[19:38]
+ConfigMoveSetVar = ""
 for permutation in MoveSetConfigs:
-    ConfigMoveSetVar+=permutation.rstrip(',\n')+','
-ConfigMoveSetVar=ConfigMoveSetVar.rstrip(',')
-ConfigMoveSet=tuple(eval(ConfigMoveSetVar))
+    ConfigMoveSetVar += permutation.rstrip(",\n") + ","
+ConfigMoveSetVar = ConfigMoveSetVar.rstrip(",")
+ConfigMoveSet = tuple(eval(ConfigMoveSetVar))
 fh.close()
 
-initialWinsLosses={"Wins":0,"Losses":0}
+initialWinsLosses = {"Wins": 0, "Losses": 0}
 
 try:
-    fh=open("Scores.dat","rb+")
+    fh = open("Scores.dat", "rb+")
 except FileNotFoundError:
-    f=open("Scores.dat","wb")
-    pickle.dump(initialWinsLosses,f)
+    f = open("Scores.dat", "wb")
+    pickle.dump(initialWinsLosses, f)
     f.close()
-    fh=open("Scores.dat","rb+")
+    fh = open("Scores.dat", "rb+")
 
-WinLoose=pickle.load(fh)
+WinLoose = pickle.load(fh)
 fh.close()
 
+
 def update(WinOrLoose):
-    f=open("Scores.dat","rb+")
-    f1=open("temp.dat","wb")
-    data=pickle.load(f)
-    if WinOrLoose=='Win':
-        newvar=data["Wins"]
-        data['Wins']=newvar+1
-        pickle.dump(data,f1)
+    f = open("Scores.dat", "rb+")
+    f1 = open("temp.dat", "wb")
+    data = pickle.load(f)
+    if WinOrLoose == "Win":
+        newvar = data["Wins"]
+        data["Wins"] = newvar + 1
+        pickle.dump(data, f1)
         f.close()
         f1.close()
-    elif WinOrLoose=='Loose':
-        newvar=data["Losses"]
-        data['Losses']=newvar+1
-        pickle.dump(data,f1)
+    elif WinOrLoose == "Loose":
+        newvar = data["Losses"]
+        data["Losses"] = newvar + 1
+        pickle.dump(data, f1)
         f.close()
         f1.close()
     os.remove("Scores.dat")
-    os.rename("temp.dat","Scores.dat")
- 
+    os.rename("temp.dat", "Scores.dat")
+
+
 class Game:
     _initial_moves = InitialMoves
 
-    _initial_board = [
-        BLACK, BLACK, BLACK,
-        EMPTY, EMPTY, EMPTY,
-        WHITE, WHITE, WHITE
-    ]
+    _initial_board = [BLACK, BLACK, BLACK, EMPTY, EMPTY, EMPTY, WHITE, WHITE, WHITE]
 
     def __init__(self):
-        self.wins = WinLoose['Wins']
-        self.losses = WinLoose['Losses']
+        self.wins = WinLoose["Wins"]
+        self.losses = WinLoose["Losses"]
         self.winner = None
         self.message = None
         self.board = deepcopy(self._initial_board)
@@ -85,15 +87,15 @@ class Game:
         self.x = self.y = 0
         self.configs = ConfigMoveSet
 
-    #Show state of board after every move
+    # Show state of board after every move
     def reset(self):
         self.board = deepcopy(self._initial_board)
         self.winner = None
         self.message = None
 
-    #Uses Machine Learning to remove losing strategies
+    # Uses Machine Learning to remove losing strategies
     def game_over(self, winner, message=None):
-        
+
         # If the player won
         if winner is WHITE:
             self.wins += 1
@@ -102,7 +104,7 @@ class Game:
             if message:
                 self.message = message
             else:
-                self.message = 'You win.'
+                self.message = "You win."
 
             # Remove losing strategy from moves list
             self.moves[self.x][self.y] = 0
@@ -114,10 +116,9 @@ class Game:
             update("Loose")
 
             if message:
-                self.message += '\n' + message
+                self.message += "\n" + message
             else:
-                self.message += '\nI win.'
-
+                self.message += "\nI win."
 
         # Winner should only ever be player or machine
         else:
@@ -126,24 +127,21 @@ class Game:
         self.winner = winner
 
     def overview(self):
-        return 'Games lost = {}, Games won = {}, Total Games = {}.'.format(
+        return "Games lost = {}, Games won = {}, Total Games = {}.".format(
             self.losses, self.wins, self.losses + self.wins
         )
 
 
-#Printing Gameboard
+# Printing Gameboard
 def fnr(x):
 
-    rval = {
-        1: 3, 3: 1,
-        4: 6, 6: 4,
-        7: 9, 9: 7
-    }
+    rval = {1: 3, 3: 1, 4: 6, 6: 4, 7: 9, 9: 7}
     return rval.get(x, x)
+
 
 # Player Move
 def white_move(m1, m2, game):
-    
+
     assert game.winner is None
     game.message = None
 
@@ -211,7 +209,8 @@ def white_move(m1, m2, game):
     # If machine has no valid moves player wins
     game.game_over(WHITE)
 
-#Machine Move
+
+# Machine Move
 def black_move(game):
 
     assert game.winner is None
@@ -245,7 +244,7 @@ def black_move(game):
 
     # If a move cannot be found, machine loses
     if not strategies:
-        game.game_over(WHITE, message='I lose.')
+        game.game_over(WHITE, message="I lose.")
         return
 
     # Get machine's move
@@ -257,7 +256,7 @@ def black_move(game):
     # Perform move
     game.board[move[0] - 1] = EMPTY
     game.board[move[1] - 1] = BLACK
-    game.message = 'I move from {} to {}'.format(move[0], move[1])
+    game.message = "I move from {} to {}".format(move[0], move[1])
 
     # Check if any machine pieces have reached end of board
     if BLACK in (game.board[6], game.board[7], game.board[8]):
@@ -283,19 +282,21 @@ def black_move(game):
     for i in (3, 4, 6, 7):
         if game.board[i] is WHITE and game.board[i - 2] is BLACK:
             return
-    game.game_over(BLACK, message='Human Loses, Machine Wins.')
+    game.game_over(BLACK, message="Human Loses, Machine Wins.")
+
 
 from tkinter import *
 
-COLOUR1 = 'black',
-COLOUR2 = 'white',
+COLOUR1 = ("black",)
+COLOUR2 = ("white",)
 TILE_SIZE = 6
 
-#Start Game
+# Start Game
 def main():
     game = Game()
     gui = GUI(game)
     gui.mainloop()
+
 
 class GUI(Tk):
     def __init__(self, game):
@@ -307,24 +308,21 @@ class GUI(Tk):
         Tk.__init__(self)
         self.title("Hexapawn")
         self.resizable(0, 0)
-        self.wpawn = PhotoImage(file='white.gif')
-        self.bpawn = PhotoImage(file='black.gif')
-        self.empty = PhotoImage(file='empty.gif')
-        self.tk.call('wm', 'iconphoto', self._w, self.wpawn)
+        self.wpawn = PhotoImage(file="white.gif")
+        self.bpawn = PhotoImage(file="black.gif")
+        self.empty = PhotoImage(file="empty.gif")
+        self.tk.call("wm", "iconphoto", self._w, self.wpawn)
 
         self.notice = StringVar()
         Label(
-            self, textvariable=self.notice, width=45, height=2,
-            font="Sans 12 bold"
+            self, textvariable=self.notice, width=45, height=2, font="Sans 12 bold"
         ).pack(pady=10)
 
         # Create game board
         self.tiles = list()
         self.tile_frame = Frame()
         self.tile_frame.pack(padx=60, pady=(0, 60))
-        self.new_game_button = Button(
-            self, text='NEW GAME', command=self.enable
-        )
+        self.new_game_button = Button(self, text="NEW GAME", command=self.enable)
         self.new_game_button.pack_forget()
 
         # Create game board tiles
@@ -337,7 +335,7 @@ class GUI(Tk):
                     relief=FLAT,
                     bg=color,
                     activebackground=color,
-                    command=lambda i=i: self.player_selected(i)
+                    command=lambda i=i: self.player_selected(i),
                 )
                 tile.grid(column=col, row=row)
                 self.tiles.append(tile)
@@ -345,7 +343,7 @@ class GUI(Tk):
                 i += 1
         self.set_pieces()
 
-    #Movement of Pawns
+    # Movement of Pawns
     def set_pieces(self):
         for i in range(9):
             piece = self.game.board[i]
@@ -361,7 +359,7 @@ class GUI(Tk):
                 self.tiles[i].config(image=self.empty)
                 self.tiles[i].image = self.empty
 
-    #Determines tile clicked on
+    # Determines tile clicked on
     def player_selected(self, position):
         if self.m1 is not None:
             self.move(self.m1, position)
@@ -370,51 +368,50 @@ class GUI(Tk):
             if self.game.board[position] is WHITE:
                 self.m1 = position
 
-    #Calculates Moves (Whether Illegeal or Not)
+    # Calculates Moves (Whether Illegeal or Not)
     def move(self, m1, m2):
         try:
             white_move(m1 + 1, m2 + 1, self.game)
         except IllegalMove:
-            self.notice.set('Illegal move.')
+            self.notice.set("Illegal move.")
             return
         except IllegalCoordinate:
-            self.notice.set('Illegal coordinates.')
+            self.notice.set("Illegal coordinates.")
             return
 
         self.set_pieces()
         if self.game.winner:
-            self.notice.set(self.game.message.replace('\n', '-- '))
-            self.notice.set(
-                '{}\n{}'.format(self.notice.get(), self.game.overview()))
+            self.notice.set(self.game.message.replace("\n", "-- "))
+            self.notice.set("{}\n{}".format(self.notice.get(), self.game.overview()))
             self.disable()
             return
 
         black_move(self.game)
         self.set_pieces()
-        self.notice.set(self.game.message.replace('\n', '-- '))
+        self.notice.set(self.game.message.replace("\n", "-- "))
 
         if self.game.winner:
-            self.notice.set(
-                '{}\n{}'.format(self.notice.get(), self.game.overview()))
+            self.notice.set("{}\n{}".format(self.notice.get(), self.game.overview()))
             self.disable()
             return
 
-    #Disables gameboard interaction
+    # Disables gameboard interaction
     def disable(self):
         for tile in self.tiles:
-            tile.config(state='disabled')
+            tile.config(state="disabled")
         self.new_game_button.pack(pady=16)
         self.tile_frame.pack(padx=60, pady=0)
 
-    #Disables gameboard interaction
+    # Disables gameboard interaction
     def enable(self):
         for tile in self.tiles:
-            tile.config(state='normal')
+            tile.config(state="normal")
         self.game.reset()
         self.set_pieces()
         self.new_game_button.pack_forget()
         self.tile_frame.pack(padx=60, pady=(0, 60))
 
-#Start game
+
+# Start game
 if __name__ == "__main__":
     main()
